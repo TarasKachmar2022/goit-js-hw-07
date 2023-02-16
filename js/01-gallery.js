07cdf1f3f90ec0838c49157baca768d8a6c0aeea
Taras Kachmar <kachmar.taras@gmail.com>
2023-02-16 20:20:45 +0200

    done Task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryEl: document.querySelector('.gallery')
+}
+
+refs.galleryEl.addEventListener('click', onGalleryCardClick);
+
+const gallaryCardsMarkup = createGallaryCardsMarkup(galleryItems);
+refs.galleryEl.innerHTML = gallaryCardsMarkup;
+
+function createGallaryCardsMarkup(items){
+    return items.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </div>`;
+}).join('');
+}
+
+function onGalleryCardClick(event){
+    event.preventDefault();
+    const source = event.target.dataset.source;
+    
+    if(event.target.classList.contains('gallery__image')){
+        document.addEventListener('keydown', onKeyModalClose);
+
+        const instance = basicLightbox.create(`
+        <img src="${source}" width="1280" height="852">
+        `)
+        instance.show();
+
+        function onKeyModalClose(event){
+            if(event.code === 'Escape'){
+                instance.close()
+            }
+        }
+    }
+    }
